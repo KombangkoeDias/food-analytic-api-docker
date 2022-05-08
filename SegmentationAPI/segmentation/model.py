a9a485ebfad5189ace6244c61c0b1e6b9a9d95ce
kbd <kbdirifilation@gmail.com>
2022-05-08 08:29:15 +0700

    naming convention for config file

--- SegmentationAPI/segmentation/model.py
+++ SegmentationAPI/segmentation/model.py
@@ -3,7 +3,7 @@ import mmcv
 from mmcv import Config
 from mmseg.apis import init_segmentor
 
-config = os.path.abspath('SegmentationAPI/segmentation/configs/configs.py')
+config = os.path.abspath('SegmentationAPI/segmentation/configs/fpn_semask_base_fp16_640x640_800k_foodseg103.py')
 checkpoint = os.path.abspath('SegmentationAPI/segmentation/checkpoints/final.pth')
 model = init_segmentor(config, checkpoint)
 
