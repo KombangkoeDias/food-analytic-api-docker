3aa20eda41a6f21bd563e665817d7f98f2714b37
Prachaya Liewtipa <peakl2000@gmail.com>
2022-05-10 00:30:14 +0700

    Take input image tensor to device

--- ClassificationAPI/model.py
+++ ClassificationAPI/model.py
@@ -3,6 +3,8 @@ import torch
 from torch import nn
 from .config import config
 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class foodNet(nn.Module):
   def __init__(self):
     super(foodNet, self).__init__()
@@ -10,10 +12,10 @@ class foodNet(nn.Module):
     self.pretrained_model.head.fc = nn.Linear(self.pretrained_model.head.fc.in_features, len(config["idx_to_class"]))
 
   def forward(self, input):
+    input = input.to(device)
     x = self.pretrained_model(input)
     return x
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = foodNet()
 model.to(device)
 model.load_state_dict(torch.load(config["model_weight_filepath"], map_location=device))
