de1f266fd49340e5e75fe85caa5469f3950bdb7b
KombangkoeDias <kbdirifilation@gmail.com>
2022-05-14 09:19:16 +0700

    add cors to the api

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 from template_api import template_api
-from DepthAPI import depth_api
+#from DepthAPI import depth_api
 from SegmentationAPI import segmentation_api
-from ClassificationAPI import classification_api
+#from ClassificationAPI import classification_api
 
 app = Flask(__name__)
 
 app.register_blueprint(template_api, url_prefix="/template")
-app.register_blueprint(depth_api, url_prefix="/depth")
+#app.register_blueprint(depth_api, url_prefix="/depth")
 app.register_blueprint(segmentation_api, url_prefix="/segmentation")
-app.register_blueprint(classification_api, url_prefix="/classification")
+#app.register_blueprint(classification_api, url_prefix="/classification")
 
 app.config['JSON_AS_ASCII'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+CORS(app)
 
 @app.route("/")
 def hello():
@@ -21,4 +23,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=1234)
+
