3cea32d7c289a5f65f59633f4971361868bb9967
KombangkoeDias <kbdirifilation@gmail.com>
2022-05-15 11:43:03 +0700

    test

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from flask import Flask
+from flask_cors import CORS
 from template_api import template_api
-from DepthAPI import depth_api
-from SegmentationAPI import segmentation_api
+# from DepthAPI import depth_api
+# from SegmentationAPI import segmentation_api
 from ClassificationAPI import classification_api
-from volume_estimationAPI import volume_estimation_api
 from coin_detectionAPI import coin_detection_api
+from volume_estimationAPI import volume_estimation_api
 
 app = Flask(__name__)
 
 app.register_blueprint(template_api, url_prefix="/template")
-app.register_blueprint(depth_api, url_prefix="/depth")
-app.register_blueprint(segmentation_api, url_prefix="/segmentation")
-app.register_blueprint(volume_estimation_api, url_prefix="/volumeEstimation")
+# app.register_blueprint(depth_api, url_prefix="/depth")
+# app.register_blueprint(segmentation_api, url_prefix="/segmentation")
+# app.register_blueprint(volume_estimation_api, url_prefix="/volumeEstimation")
 app.register_blueprint(classification_api, url_prefix="/classification")
 app.register_blueprint(coin_detection_api, url_prefix="/coinDetection")
 
 app.config['JSON_AS_ASCII'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+CORS(app)
 
 @app.route("/")
 def hello():
@@ -25,4 +27,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=1234)
